76ce1985bc64220306767803fd8de9d820e69bdd
Hiroya Onoe <git@onoe.dev>
2022-12-07 13:40:11 +0900

    Remove commands

--- cmd/bcop-proxy/main.go
+++ cmd/bcop-proxy/main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"flag"
+	"sync"
 
+	"github.com/hiroyaonoe/bcop-proxy/admin/api/http"
+	"github.com/hiroyaonoe/bcop-proxy/admin/api/http/controller"
+	"github.com/hiroyaonoe/bcop-proxy/admin/usecase"
 	"github.com/hiroyaonoe/bcop-proxy/proxy"
 	"github.com/hiroyaonoe/bcop-proxy/repository/inmemory"
 )
 
 func main() {
+	adminPort := flag.String("admin-port", "9001", "admin listen port")
+
+	proxyPort := flag.String("proxy-port", "9000", "proxy listen port")
 	propagate := flag.Bool("propagate", true, "header propagation?")
 	defaultAddr := flag.String("default-addr", "", "defalut address")
-	port := flag.String("port", "8081", "listen port")
+
 	flag.Parse()
 
 	repoEnv := inmemory.NewEnv()
+
+	ucEnv := usecase.NewEnv(repoEnv)
+	ctrlEnv := controller.NewEnv(ucEnv)
+	router := http.NewRouter(ctrlEnv)
+	router.Set()
+
 	server := &proxy.Server{
 		Env:                         repoEnv,
 		GetEnvIDFromHeaderValueFunc: proxy.GetEnvIDFromBaggage,
@@ -21,5 +34,10 @@ func main() {
 		DefaultAddr:                 *defaultAddr,
 	}
 
-	server.Start(":" + *port)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go router.Run(":" + *adminPort)
+	go server.Start(":" + *proxyPort)
+	wg.Wait()
 }
