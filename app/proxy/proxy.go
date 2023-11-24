56ee3930d86b0a57dd7679f05111c5d65f53f250
Hiroya Onoe <git@onoe.dev>
2023-11-24 19:58:29 +0900

    fix proxy

--- app/proxy/proxy.go
+++ app/proxy/proxy.go
@@ -3,20 +3,19 @@ package proxy
 import (
 	"io"
 	"net"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func proxy(client, server net.Conn) error {
-	var eg errgroup.Group
+	errCh := make(chan error, 2)
 
-	eg.Go(func() error { return relay(client, server) })
-	eg.Go(func() error { return relay(server, client) })
+	go relay(errCh, client, server)
+	go relay(errCh, server, client)
 
-	return eg.Wait()
+	return <-errCh
 }
 
-func relay(src io.Reader, dst io.Writer) error {
+func relay(errCh chan error, src io.Reader, dst io.Writer) {
 	_, err := io.Copy(dst, src)
-	return err
+	errCh <- err
+	return
 }
